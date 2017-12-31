ce44d353e896d70bb22cc011c32374bba20a29a8
Anton <Anton@Voron-note>
2017-12-31 18:40:30 +0400

    январь 2018

--- EverydayBibleReading/src/ru/itprospect/everydaybiblereading/WorkSettings.java
+++ EverydayBibleReading/src/ru/itprospect/everydaybiblereading/WorkSettings.java
@@ -17,12 +17,12 @@ public class WorkSettings {
 		
 		//Проверка наличия хмл для даты и конфессии
 		if (conf.equals(CONF_ORTHODOXY)) {
-			if (date.compareTo(new GregorianCalendar(2013, 2, 1))>=0 && date.compareTo(new GregorianCalendar(2017, 11, 31))<=0) {
+			if (date.compareTo(new GregorianCalendar(2013, 2, 1))>=0 && date.compareTo(new GregorianCalendar(2018, 11, 31))<=0) {
 				fileFindSuccess = true;
 			}
 		} 
 		else if (conf.equals(CONF_CATHOLIC)) {
-			if (date.compareTo(new GregorianCalendar(2013, 11, 1))>=0 && date.compareTo(new GregorianCalendar(2017, 11, 31))<=0) {
+			if (date.compareTo(new GregorianCalendar(2013, 11, 1))>=0 && date.compareTo(new GregorianCalendar(2018, 0, 4))<=0) {
 				fileFindSuccess = true;
 			}			
 		};
@@ -59,6 +59,9 @@ public class WorkSettings {
 			}
 			else if (date.compareTo(new GregorianCalendar(2017, 0, 1))>=0 && date.compareTo(new GregorianCalendar(2017, 11, 31))<=0) {
 				return R.xml.read_cal_ru_cat_2017;
+			}
+			else if (date.compareTo(new GregorianCalendar(2018, 0, 1))>=0 && date.compareTo(new GregorianCalendar(2018, 11, 31))<=0) {
+				return R.xml.read_cal_ru_cat_2018;
 			}
 		};		
 		
